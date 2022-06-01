d5b6ae1a4e8fd18cdcf1158a625f25ed52150947
Kevin Bell <bellKevin@pm.me>
2022-06-01 01:08:14 +0000

    Update SavingsAccount.java

--- savingsAccount/src/savingsaccount/SavingsAccount.java
+++ savingsAccount/src/savingsaccount/SavingsAccount.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class SavingsAccount {
 
-    private double balance = 0;
-    private double interestRate = 0;
+    private double balance = 0.0;
+    private double interestRate;
 
-    public void setInterestRate() {
-        this.interestRate = interestRate;
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate / 100.0;
     }
 
-    public void setBalance() {
+    public void setBalance(double balance) {
         this.balance = balance;
     }
 
@@ -19,7 +19,9 @@ public class SavingsAccount {
         return this.balance;
     }
 
-    public void growAccount() {
-
+    public void growAccount(int cycles) {
+        for (int i = 0; i < cycles; i++) {
+            this.balance += (this.interestRate * this.balance);
+        }
     }
 }
